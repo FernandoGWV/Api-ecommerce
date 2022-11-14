da8f24ce60aaa3baff79f45540136f2b3d557211
Luiz <luizwinther09@gmail.com>
2022-11-14 07:14:32 -0300

    finalizado por completo

--- services/useMedia.tsx
+++ services/useMedia.tsx
@@ -8,6 +8,7 @@ const useMedia = (media: string) => {
       const { matches } = window.matchMedia(media);
       setMatch(matches);
     };
+    changeMatch();
 
     window.addEventListener("resize", changeMatch);
 
